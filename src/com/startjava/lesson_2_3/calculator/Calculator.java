afa32357d29dc100ad48617d56e9c83da756786a
Oleg Razin <kekcoe@gmail.com>
2021-01-14 11:49:25 +0300

    Was adding array

--- src/com/startjava/lesson_2_3/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3/calculator/Calculator.java
@@ -1,54 +1,27 @@
 package com.startjava.lesson_2_3.calculator;
 
- public class Calculator {
-	
-		private int firstNumb;
-		private int secondNumb;
-		private char sign;
+public class Calculator {
+	 private  int firstNumb;
+	 private  int secondNumb;
+	 private char sign;
 
-		public void setFirstNumb(int firstNumb) {
-			this.firstNumb = firstNumb;
+	 public Calculator(String mathExpression) {
+		 String[] arrExpression = mathExpression.split(" ");
+		 firstNumb = Integer.parseInt(arrExpression[0]);
+		 sign = arrExpression[1].charAt(0);
+		 secondNumb = Integer.parseInt(arrExpression[2]);
+	 }
+
+		 public void calculate() {
+			 switch (sign) {
+				 case '+' -> System.out.println(firstNumb + " + " + secondNumb + " = " + Math.addExact(firstNumb, secondNumb));
+				 case '-' -> System.out.println(firstNumb + " - " + secondNumb + " = " + (firstNumb - secondNumb));
+				 case '*' -> System.out.println(firstNumb + " * " + secondNumb + " = " + (firstNumb * secondNumb));
+				 case '/' -> System.out.println(firstNumb + " / " + secondNumb + " = " + Math.floorDiv(firstNumb, secondNumb));
+				 case '^' -> System.out.println(firstNumb + " ^ " + secondNumb + " = " + Math.pow(firstNumb, secondNumb));
+				 case '%' -> System.out.println(firstNumb + " % " + secondNumb + " = " + (firstNumb % secondNumb));
+				 default -> System.out.println("Введи знак мат.операции " + " + " + " - " + " * " + " / " + " ^ " + " % ");
+			 }
 		}
 
-		public void setSecondNumb(int secondNumb) {
-			this.secondNumb = secondNumb;
-		}
-
-		public void setSign(char sign) {
-			this.sign = sign;
-		}
-
-		public void calculate() {
-			switch(sign) {
-				case '+':
-					System.out.println(firstNumb + " + " + secondNumb + " = " + (firstNumb + secondNumb));
-					break;
-				case '-':
-					System.out.println(firstNumb + " - " + secondNumb + " = " + (firstNumb - secondNumb));
-					break;
-				case '*':
-					System.out.println(firstNumb + " * " + secondNumb + " = " + (firstNumb * secondNumb));
-					break;
-				case '/':
-					System.out.println(firstNumb + " / " + secondNumb + " = " + (firstNumb / secondNumb));
-					break;
-				case '^':
-					System.out.println(firstNumb + " ^ " + secondNumb + " = " + pow(firstNumb, secondNumb));
-					break;
-				case '%':
-					System.out.println(firstNumb + " % " + secondNumb + " = " + (firstNumb % secondNumb));
-					break;
-				default:
-					System.out.println("Введи знак мат.операции " + " + " + " - " + " * " + " / " + " ^ " + " % ");
-			}
-		}
-
-		//exponentiation
-		private int pow(int firstNumb, int secondNumb) {
-			int result = 1;
-			for (int i = 0; i < secondNumb; i++) {
-				result *= firstNumb;
-			}
-			return result;
-		}
-	}
+}
--- src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
@@ -1,30 +1,25 @@
 package com.startjava.lesson_2_3.calculator;
-import com.startjava.lesson_2_3.calculator.Calculator;
 import java.util.Scanner;
 
-class CalculatorTest {
-	public static void main(String[] args) {
-		Calculator calc = new Calculator();
+class CalculatorTest  {
+	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
-
+		Calculator calc = new Calculator(sc.nextLine());
 		String userAnswer = "yes";
+
 		while(userAnswer.equals("yes")) {
-			System.out.print("Введите первое число: ");
-			calc.setFirstNumb(sc.nextInt());
+			do {
+				System.out.println("Введите  выражение по типу a + b через пробел: ");
+			} while(sc.nextLine().equals(""));
 
-			System.out.print("Введите знак математической операции: ");
-			calc.setSign(sc.next().charAt(0));
-
-			System.out.print("Введите второе число: ");
-			calc.setSecondNumb(sc.nextInt());
-
-			System.out.println("");
-			
 			calc.calculate();
+
 			do {
 				System.out.print("Хотите продолжить вычисления? [yes/no]: ");
 				userAnswer = sc.next();
 			} while(!userAnswer.equals("yes") && !userAnswer.equals("no"));
 		}
+		sc.close();
 	}
+
 }
