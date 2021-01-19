fff64df3521834a900bc3aab1261868db4ba7583
Oleg Razin <kekcoe@gmail.com>
2021-01-19 16:04:48 +0300

    Was edited comments from 19Jan

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -8,8 +8,7 @@ public class GuessNumber {
     private int secretNumber;
     private final Player player1;
     private final Player player2;
-    private int playerTry;
-    private int playerLastNumber;
+    private int count;
 
     GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -28,43 +27,48 @@ public class GuessNumber {
             if (compareNumber(player2)) {
                 break;
             }
-            playerTry++;
-            if (playerTry == 10) {
+            count++;
+            player1.setAttempt(count);
+            player2.setAttempt(count);
+            if (count == 10) {
                 System.out.println(" Yours tries has over");
                 break;
             }
         } while (true);
-
-        showPlayerNumbers(player1, playerTry);
-        showPlayerNumbers(player2, playerTry);
-        playerTry = 0;
-        Arrays.fill(player1.getTries(), 0);
-        Arrays.fill(player2.getTries(), 0);
+        showPlayerNumbers(player1);
+        showPlayerNumbers(player2);
+        count = 0;
+       fillZero(player1);
+       fillZero(player2);
     }
 
     public void inputNumber(Player player) {
         Scanner sc = new Scanner(System.in);
         System.out.println(player.getName() + " input your number ");
-        player.setPlayerTries(playerTry, sc.nextInt());
+        player.setTries(count, sc.nextInt());
     }
 
     public boolean compareNumber(Player player) {
-        playerLastNumber = (player.getTries()[playerTry]);
-        if (playerLastNumber == secretNumber) {
-            System.out.println("Player " + player.getName() + " guessed the number from " + (playerTry + 1) + " try");
+        player.setLastNumber((player.getTries()[count]));
+        if (player.getLastNumber() == secretNumber) {
+            System.out.println("Player " + player.getName() + " guessed the number from " + (player.getAttempt() + 1) + " try");
             return true;
         }
-        String result = playerLastNumber > secretNumber ? "больше" : "меньше";
+        String result = player.getLastNumber() > secretNumber ? "больше" : "меньше";
         System.out.println(player.getName() + " число введенное вами " + result + " того, что загадал компьютер");
         return false;
     }
 
-    private void showPlayerNumbers(Player player, int lengthTries) {
+    private void showPlayerNumbers(Player player) {
         System.out.println(player.getName());
-        for (int i = 0; i <= lengthTries; i++) {
-            System.out.print(player.getTries()[i] + " ");
-        }
+        player.getTries();
+        player.outputTries(player.getTries(), player.getAttempt());
         System.out.println();
     }
 
+    public void fillZero(Player player){
+         for(int i = 0; i <= player.getAttempt(); i ++) {
+             Arrays.fill(player.getTries(), 0);
+        }
+    }
 }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -1,11 +1,23 @@
 package com.startjava.lesson_2_3_4.game;
 
+import java.util.Arrays;
+
 class Player {
     private String name;
     private int[] tries = new int[10];
+    int lastNumber;
+    int attempt;
 
     public Player(String name) {
         this.name = name;
+    }
+
+    public int getLastNumber() {
+        return lastNumber;
+    }
+
+    public void setLastNumber(int lastNumber) {
+        this.lastNumber = lastNumber;
     }
 
     public String getName() {
@@ -16,7 +28,21 @@ class Player {
         return tries;
     }
 
-    public void setPlayerTries(int index, int userNumber) {
-        tries[index] = userNumber;
+    public void setTries(int index, int playerNumber) {
+        tries[index] = playerNumber;
+    }
+
+    public int getAttempt() {
+        return attempt;
+    }
+
+    public void setAttempt(int attempt) {
+        this.attempt = attempt;
+    }
+
+    public void outputTries(int[] tries, int attempt) {
+        for (int number : Arrays.copyOf(tries, attempt + 1)) {
+            System.out.print(number + " ");
+        }
     }
 }
