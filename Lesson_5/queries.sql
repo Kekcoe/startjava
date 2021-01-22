6312936a0523c8b8bb468b065198b5438d87c509
Oleg Razin <kekcoe@gmail.com>
2021-01-22 09:54:37 +0300

    TExt was formatted

--- Lesson_5/queries.sql
+++ Lesson_5/queries.sql
@@ -1,34 +1,25 @@
-SELECT * 
-	FROM jaegers;
+SELECT * FROM jaegers;
 
-SELECT * 
-	FROM jaegers 
-	WHERE status = 'active';
+SELECT * FROM jaegers 
+WHERE status = 'active';
 
-SELECT * 
-	FROM jaegers
-	WHERE mark = 3 
-	OR mark = 5;
+SELECT * FROM jaegers 
+WHERE mark = 3 OR mark = 5;
 
-SELECT * 
-	FROM jaegers 
-	ORDER BY mark DESC;
+SELECT * FROM jaegers 
+ORDER BY mark DESC;
 
-SELECT modelName 
-		FROM jaegers
-		WHERE launch = (SELECT MIN(launch) FROM jaegers);
+SELECT modelName FROM jaegers 
+WHERE launch = (SELECT MIN(launch) FROM jaegers);
 
-SELECT modelName 
-		FROM jaegers
-		WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM jaegers);
+SELECT modelName FROM jaegers 
+WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM jaegers);
 
-SELECT AVG(weight) 
-	FROM jaegers;
+SELECT AVG(weight) FROM jaegers;
 
 UPDATE jaegers 
-	SET kaijuKill = kaijuKill + 1 
-	WHERE status = 'active';
+SET kaijuKill = kaijuKill + 1 
+WHERE status = 'active';
 
-DELETE 
-	FROM jaegers 
-	WHERE status = 'Destroyed';
+DELETE FROM jaegers 
+WHERE status = 'Destroyed';
